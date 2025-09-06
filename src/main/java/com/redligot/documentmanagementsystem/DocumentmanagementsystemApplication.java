dd17f15ca89e78df61e24ac05abf93f5b0641a63
redligot2009 <redligot2009.forreal@gmail.com>
2025-09-06 20:19:40 +0800

    Initial Commit

---
+++ src/main/java/com/redligot/documentmanagementsystem/DocumentmanagementsystemApplication.java
@@ -0,0 +1,13 @@
+package com.redligot.documentmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentmanagementsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentmanagementsystemApplication.class, args);
+	}
+
+}
